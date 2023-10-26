27ae1746550330151556ab3aacdcf6c17e6eea37
rajeshchauhan1426 <rc810609@gmail>
2023-10-26 17:36:23 +0530

    "new "

---
+++ main.java
@@ -0,0 +1,20 @@
+import java.util.Stack;
+
+
+public class main {
+    public static void main(String[] args){
+        Stack<String> stack = new Stack<String>();
+        
+
+
+        stack.push("minecraft" );
+        stack.push("college" );
+        stack.push("DSA" );
+        //System.out.println(stack.empty());
+
+        //stack.pop();
+        System.out.println(stack.peek());
+        System.out.println(stack);
+    }
+     
+}
