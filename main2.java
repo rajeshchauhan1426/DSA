5e81ef41ab9fbb778eeb5f65d68fe590f643aeeb
rajeshchauhan1426 <rc810609@gmail>
2023-10-31 18:45:46 +0530

    "Queue"

--- main2.java
+++ main2.java
@@ -1,26 +1,27 @@
 import java.util.*;
+import java.util.PriorityQueue;
+import java.util.Queue;
+import java.util.Collection;
 
 public class main2 {
 
-    public static void main(String args[]) {
-        // Priority Queue = A FIFO data structure that serves elements
-        // with the highest priorities first
-        // before elements with lower priority
+    public static void main(String args[]){
+        // Queue <Double> queue = new PriorityQueue<>();
+        Queue <String> queue = new PriorityQueue<>();
 
-        // Strings in default order
-        // Queue<String> queue = new PriorityQueue<>();
 
-        // Strings in reverse order
-        Queue<String> queue = new PriorityQueue<>();
+        // queue.offer(1.2);
+        // queue.offer(1.5);
+        // queue.offer(1.1);
 
-        queue.offer("B");
-        queue.offer("C");
         queue.offer("A");
-        queue.offer("F");
-        queue.offer("D");
+        queue.offer("d");
+        queue.offer("c");
+        queue.offer("s");
 
         while (!queue.isEmpty()) {
             System.out.println(queue.poll());
+            
         }
     }
 }
